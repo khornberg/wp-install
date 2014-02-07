125ed1bd0ca81e4b64ffda183c0eb0972f722a18
Kyle Hornberg <kyle.hornberg@gmail.com>
2014-02-06 20:55:08 -0600

    Fixes

--- wp-install.sh
+++ wp-install.sh
@@ -10,19 +10,19 @@
 # Install dependencies on Ubuntu
 PHPVERSION=5.4.
 echo "Checking for PHP $PHPVERSION"
-dpkg-query -l php5 | grep $PHPVERSION
+dpkg-query -l php5 | grep -q $PHPVERSION
 
 if [ $? -eq 0 ]; then
-	echo "Installed"
+	echo "$PHPVERSION Installed"
 else
 	echo "Installing PHP 5"
 	sudo apt-get install php5
 fi
 
-dpkg-query -l php5-sqlite | grep not-installed
+dpkg-query -l php5-sqlite | grep -q ii
 
 if [ $? -eq 0 ]; then
-	echo "Installed"
+	echo "php-sqlite Installed"
 else
 	echo "Installing php5-sqlite"
 	sudo apt-get install php5-sqlite
@@ -30,6 +30,7 @@ fi
 
 # Get WordPress
 git clone https://github.com/WordPress/WordPress.git
+mv WordPress/wp-config-sample.php WordPress/wp-config.php
 
 # Install SQLite Integration
 curl http://downloads.wordpress.org/plugin/sqlite-integration.1.5.zip --create-dirs -o WordPress/wp-content/plugins/sqlite-integration.1.5.zip
@@ -38,9 +39,12 @@ rm WordPress/wp-content/plugins/sqlite-integration.1.5.zip
 cp WordPress/wp-content/plugins/sqlite-integration/db.php WordPress/wp-content/plugins/
 
 # Get WordPress Router
-git clone https://gist.github.com/3801087.git WordPress
+git clone https://gist.github.com/3801087.git
+cp 3801087/router.php WordPress
+rm -rf 3801087
 
-if [ $1 -eq "-s"]; then
+if [ $1 == "-s" ]; then
 	# Start the server
-	php -S localhost:7000 -t WordPress router.php
+	cd WordPress
+	php -S localhost:7000 router.php
 fi
